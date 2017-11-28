76081ba197fe92b1d1ff3ce45b0766f50852f91e
Artem Samofalov <dex157@gmail.com>
2017-11-28 14:23:39 +0300

    add step 2

--- src/api.js
+++ src/api.js
@@ -1,21 +1,31 @@
-let ACCESS_TOKEN = null;
+import axios from 'axios';
 
-export const setTokenToApi = token => {
-  console.log('setTokenToApi', token);
-  ACCESS_TOKEN = token;
+const instance = axios.create({
+  baseURL: 'https://api.github.com/',
+});
+
+export const setTokenApi = access_token => {
+  instance.defaults.params = {access_token};
 };
 
-function request(url) {
-  console.log('request', url);
-  if (ACCESS_TOKEN === null) return Promise.reject('Отсутствует access_token src/api.js');
+export const clearTokenApi = () => {
+  instance.defaults.params = {access_token: undefined};
+};
 
-  return fetch(`${url}?access_token=${ACCESS_TOKEN}`, {
-    method: 'GET',
-    mode: 'cors',
-  }).then(response => response.json());
-}
+export const getUserInformation = login => instance(`users/${login}`);
+export const getUserFollowers = login => instance(`users/${login}/followers?pages=1&per_page=100`);
+export const getUserRepos = login => instance(`users/${login}/repos`);
 
-export const getUserInformation = login => request(`https://api.github.com/users/${login}`);
+export const getSignInUrl = () =>
+  `https://github.com/login/oauth/authorize?client_id=cc8c66d996c2c8478c2d`;
 
-export const getUserFollowers = login =>
-  request(`https://api.github.com/users/${login}/followers?pages=1&per_page=100`);
+export const sendCode = code =>
+  instance.post('https://github.com/login/oauth/access_token', {
+    withCredentials: true,
+    params: {
+      client_id: 'cc8c66d996c2c8478c2d',
+      client_secret: '8e64e947a95338c768662a30d6704477de4ab6bc',
+      code,
+      redirect_uri: 'http://localhost:3000/authorize_token',
+    },
+  });
